a668ce62ecaae0098193d56ef5c3bd48106acc71
William Oldham <wegg7250@gmail.com>
2018-01-12 21:41:52 +0000

    Initial

---
+++ src/test/java/io/github/binaryoverload/jsonmigrate/TestMoveMigration.java
@@ -0,0 +1,68 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2018 William Oldham
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.github.binaryoverload.jsonmigrate;
+
+import io.github.binaryoverload.JSONConfig;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class TestMoveMigration extends JsonMigration {
+
+    public TestMoveMigration() throws FileNotFoundException {
+        super(new JSONConfig(JSONConfig.class.getClassLoader().getResourceAsStream("test.json")));
+    }
+
+    @Override
+    public void migrate() {
+        movePath("items.properties.id", "items.properties.items");
+    }
+
+    @Override
+    public void reverseMigration() {
+        movePath("items.properties.items", "items.properties.id");
+    }
+
+    @Test
+    public void test() {
+        assertTrue(getConfig().getElement("items.properties.id").isPresent());
+        assertFalse(getConfig().getElement("items.properties.items").isPresent());
+
+        migrate();
+
+        assertFalse(getConfig().getElement("items.properties.id").isPresent());
+        assertTrue(getConfig().getElement("items.properties.items").isPresent());
+
+        reverseMigration();
+
+        assertTrue(getConfig().getElement("items.properties.id").isPresent());
+        assertFalse(getConfig().getElement("items.properties.items").isPresent());
+
+    }
+
+}
